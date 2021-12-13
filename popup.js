45235b5398ee2c615407dfd0b2765194a5e6913c
Yauheni Charniauski <yauhenicharniauski@Yauhenis-MacBook-Pro-2.local>
2021-12-13 10:58:53 +0300

    Copy token to buffer

--- popup.js
+++ popup.js
@@ -1,9 +1,8 @@
 const form = document.getElementById("control-row");
 const go = document.getElementById("go");
-const fromDomainInput = document.getElementById("from_domain");
 const toDomainInput = document.getElementById("to_domain");
-const keyPatternInput = document.getElementById("key_pattern");
-const message = document.getElementById("message");
+const success = document.getElementById("success");
+const failed = document.getElementById("failed");
 
 function getStorageSyncData(keys) {
   // Immediately return a promise and start asynchronous work
@@ -35,81 +34,63 @@ function setStorageSyncData(data) {
   });
 }
 
-// The async IIFE is necessary because Chrome <89 does not support top level await.
+function copyToBuffer(text){
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = text;
+  document.body.appendChild(copyFrom);
+  copyFrom.focus();
+  document.execCommand('SelectAll');
+  document.execCommand('Copy');
+  document.body.removeChild(copyFrom);
+}
+
+function setSuccess(str) {
+  success.textContent = str;
+  success.hidden = false;
+}
+function setFailed(str) {
+  failed.textContent = str;
+  failed.hidden = false;
+}
+
+function clearMessages() {
+  failed.textContent = '';
+  failed.hidden = true;
+  success.textContent = '';
+  success.hidden = true;
+}
+
+async function copyCookiesValues() {
+  clearMessages();
+
+  const [{ url }] = await chrome.tabs.query({active: true })
+  const cookie = await chrome.cookies.get({ url, name: '__bfx_token' });
+
+  
+  if (cookie) {
+    const toDomain = toDomainInput.value || '';
+    copyToBuffer(`${toDomain}${cookie.value}`)  
+    setSuccess('COPIED!')
+  } else {
+    setFailed(`Failed: __bfx_token cookie not found`);   
+  }
+  setTimeout(clearMessages, 1000)
+}
+
 (async function initPopupWindow() {
-  const savedValues = await getStorageSyncData(['from_domain', 'to_domain', 'key_pattern'])
-  fromDomainInput.value = savedValues.from_domain || '';
+  const savedValues = await getStorageSyncData(['to_domain'])
   toDomainInput.value = savedValues.to_domain || '';
-  keyPatternInput.value = savedValues.key_pattern || '';
-  fromDomainInput.focus();
+  copyCookiesValues()
 })();
 
 form.addEventListener("submit", handleFormSubmit);
 
 async function handleFormSubmit(event) {
   event.preventDefault();
-  clearMessage();
-  const fromDomain = fromDomainInput.value || '';
-  const toDomain = toDomainInput.value || '';
-  const pattern = keyPatternInput.value || '';
-  await setStorageSyncData({
-    from_domain: fromDomain,
-    to_domain: toDomain,
-    key_pattern: pattern,
+
+  setStorageSyncData({
+    to_domain: toDomainInput.value || '',
   })
-  const message = await copyCookiesValues(fromDomain, toDomain, pattern)
-  setMessage(message);
-}
 
-async function copyCookiesValues(fromDomain, toDomain, pattern) {
-  const cookies = await chrome.cookies.getAll({ domain: fromDomain });
-  const matchedCookies = cookies
-    .filter((cookie) => {
-      if (!pattern) return true;
-      return new RegExp('(__bfx_token).*').test(cookie.name);
-    })
-
-  const pending = matchedCookies.map((cookie) => copyCookie(cookie, toDomain));
-  await Promise.all(pending);
-
-  return `Copied ${matchedCookies.length} cookies`;
-}
-
-function stringToUrl(input) {
-  // Start with treating the provided value as a URL
-  try {
-    return new URL(input);
-  } catch {}
-  // If that fails, try assuming the provided input is an HTTP host
-  try {
-    return new URL("http://" + input);
-  } catch {}
-  // If that fails ¯\_(ツ)_/¯
-  return null;
-}
-
-function copyCookie(cookie, domain) {
-  const protocol = domain === 'localhost' ? "http:" : "https:";
-
-  // Note that the final URL may not be valid. The domain value for a standard cookie is prefixed
-  // with a period (invalid) while cookies that are set to `cookie.hostOnly == true` do not have
-  // this prefix (valid).
-  // https://developer.chrome.com/docs/extensions/reference/cookies/#type-Cookie
-  const cookieUrl = `${protocol}//${domain}${cookie.path}`;
-
-  return chrome.cookies.set({
-    url: cookieUrl,
-    name: cookie.name,
-    value: cookie.value,
-  });
-}
-
-function setMessage(str) {
-  message.textContent = str;
-  message.hidden = false;
-}
-
-function clearMessage() {
-  message.hidden = true;
-  message.textContent = "";
+  copyCookiesValues();
 }
