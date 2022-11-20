84f2ed5592017d4076269e9c66b9c45bf0c3ba9d
Eugene Charniauski <yauheni.charniauski@gmail.com>
2022-11-20 12:37:06 +0800

    x

--- popup.js
+++ popup.js
@@ -75,15 +75,25 @@ function setFailed(str) {
   setTimeout(clearMessages, 1000)
 }
 
+async function getCurrentTab() {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+  return tab
+}
+
+async function getToken() {
+  const { url } = await getCurrentTab()
+  const cookie = await chrome.cookies.get({ url, name: '__bfx_token' });
+  if (!cookie) throw new Error('__bfx_token cookie not found')
+  return cookie.value
+}
+
 async function copyCookiesValues() {
   clearMessages();
 
   try {
-    const [{ url }] = await chrome.tabs.query({ active: true, currentWindow: true })
-    const cookie = await chrome.cookies.get({ url, name: '__bfx_token' });
-    if (!cookie) throw new Error('__bfx_token cookie not found')
+    const token = await getToken() 
     const toDomain = toDomainInput.value || '';
-    copyToBuffer(`${toDomain}${cookie.value}`)  
+    copyToBuffer(`${toDomain}${token}`)  
     setSuccess('COPIED!')
   } catch (error) {
     setFailed(`Failed: ${error.message}`);   
@@ -101,7 +111,7 @@ function handleFormSubmit(event) {
 form.addEventListener("submit", handleFormSubmit);
 
 
-function setLangs() {
+function addLanguages() {
   const langs = ['en', 'ru', 'tr-TR', 'es-EM', 'pt-BR', 'zh-TW', 'zh-CN']
   langs.forEach(lang => {
     const btn = document.getElementById(lang)
@@ -143,13 +153,79 @@ function setCopyContent() {
   }
 }
 
-function initPopupWindow() {  
+function apiPost({ token, url, data }) {
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open("POST", url, true);
+    req.setRequestHeader('bfx-token', token);
+    req.setRequestHeader('content-type','application/json;charset=UTF-8');
+    req.onreadystatechange = function() {
+      if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200) {
+          resolve()
+        } else {
+          reject()
+        }
+      }
+    }
+    req.send(data);
+  })
+}
+
+async function setSetting(data) {
+  const token = await getToken()
+  if (!token) throw new Error('__bfx_token cookie not found')
+
+  try {
+    await new Promise((resolve, reject) => {
+      apiPost({ token, url: "https://api.staging.bitfinex.com/v2/auth/w/settings/set", data })
+        .then(resolve)
+        .catch(() => {
+          apiPost({ token, url: "https://api.bitfinex.com/v2/auth/w/settings/set", data })
+            .then(resolve)
+            .catch(() => reject())
+        })
+    })
+    setSuccess('Done')
+  } catch {
+    setFailed('Failed')
+  }
+
+  const { id } = await getCurrentTab()
+  await chrome.tabs.reload(id)
+}
+
+
+function addSettings () {
+  const settings = {
+    'sat-mode': '{"settings":{"api:bitfinex_bitcoinViewMode":"SAT"}}',
+    'bit-mode': '{"settings":{"api:bitfinex_bitcoinViewMode":"BIT"}}',
+    'btc-mode': '{"settings":{"api:bitfinex_bitcoinViewMode":"BTC"}}',
+    'dark-theme': '{"settings":{"api:bitfinex_theme_with_mode":"default-theme:dark-mode:default","api:bitfinex_theme":"dark-theme","api:bitfinex_color-saturation-adjustment":100,"api:bitfinex_color-contrast-adjustment":100,"api:bitfinex_color-brightness-adjustment":100}}',
+    'light-theme': '{"settings":{"api:bitfinex_theme_with_mode":"default-theme:light-mode:default","api:bitfinex_theme":"light-theme","api:bitfinex_color-saturation-adjustment":100,"api:bitfinex_color-contrast-adjustment":100,"api:bitfinex_color-brightness-adjustment":100}}',
+    'colourblind-theme': '{"settings":{"api:bitfinex_theme_with_mode":"colourblind-theme:light-mode:default","api:bitfinex_theme":"light-theme","api:bitfinex_color-saturation-adjustment":100,"api:bitfinex_color-contrast-adjustment":100,"api:bitfinex_color-brightness-adjustment":100}}',
+  }
+  
+  const keys = Object.keys(settings)
+  keys.forEach((key) => {
+    const btn = document.getElementById(key)
+    const value = settings[key]
+    btn.addEventListener('click', () => {
+      setSetting(value)
+      clickEffect(btn)
+    })
+  })
+}
+
+async function initPopupWindow() {  
   success.hidden = true;
   failed.hidden = true;
 
-  setLangs()
   setCopyContent()
   setLinks()
+  
+  addLanguages()
+  addSettings()
 }
 
 initPopupWindow();
