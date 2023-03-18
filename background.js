92714a36962ab8b1533e3b6161402d84d30d9c45
Eugene Charniauski <yauheni.charniauski@gmail.com>
2023-03-18 21:06:31 +0800

    bg improvement

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+function tryRedirectToGmail(tabId) {
+  chrome.cookies.getAll({ domain: '.mail.google.com'
+  }, function (cookies) {
+      if (cookies.length > 0) {
+        setTimeout(async () => {
+          chrome.tabs.update(tabId, { url: 'https://mail.google.com/mail/u/0/#inbox' })
+        }, 5000)
+      }
+  });
+}
+
+
 chrome.tabs.onUpdated.addListener(
   function(id, { status }, tab) {
     if (status !== 'complete') return
@@ -7,12 +19,13 @@ chrome.tabs.onUpdated.addListener(
         {
           target: {tabId: id },
           func: () => {
-              document.getElementsByName('email')[0].value='eugene.charniauski@bitfinex.com' // Your email
-              document.getElementsByClassName('AuthFormLogin')[0].submit()
+            document.getElementsByName('email')[0].value='eugene.charniauski@bitfinex.com' // Your email
+            document.getElementsByClassName('AuthFormLogin')[0].submit()
           },
         }
       );
-      setTimeout(async () => { chrome.tabs.update(id, { url: 'https://mail.google.com/mail/u/0/#inbox' }) }, 5000)
+
+      tryRedirectToGmail(id)
     }
   }
 );
